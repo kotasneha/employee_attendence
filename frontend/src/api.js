dc7abdfef084c784392f5a3829117c8744787801
kotasneha <kota.sneha29@gmail.com>
2025-11-30 18:30:02 +0530

    Updated project with latest changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://employee-attendence-a9fc.onrender.com/api'
 });
 
 export const setAuthToken = (token) => {
